ef95882b12f8610cefc5fbb889795cf672a44f28
Daniel Sont <dan.sont@gmail.com>
2018-12-24 17:11:17 +0700

    got babel7 and ava working in tests

--- test.js
+++ test.js
@@ -1,18 +1,27 @@
 import { WebSocket, Server } from 'mock-socket';
-
-
-const wsURL = "ws://whatever/ws"
-
 global.WebSocket = WebSocket;
-const mockServer = new Server(wsUrl);
+const url = "ws://whatever/ws"
 
+const mockServer = new Server(url);
 mockServer.on('connection', socket => {
     socket.on('message', data => {
         socket.send('test message from mock server');
     });
 });
 
+import test from 'ava';
 
+import { Delimited, JSON } from "./serialization";
 
-describe('socket', () => {
-})
+test('delimited', t => {
+    let p = new Delimited()
+
+    let msg = p.serialize({ type: "a", data: "b", id: "c"})
+    console.log(msg)
+	t.pass();
+});
+
+test('bar', async t => {
+	const bar = Promise.resolve('bar');
+	t.is(await bar, 'bar');
+});
